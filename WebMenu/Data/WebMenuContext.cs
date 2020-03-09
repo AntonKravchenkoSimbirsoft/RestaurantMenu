3e09b6de3d37d736309d33b6a28898b472c4ffa5
Администратор <@WIN-FSGS231BI64>
2020-03-09 16:46:12 +0400

    Добавьте файлы проекта.

---
+++ WebMenu/Data/WebMenuContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebMenu.Models;
+
+namespace WebMenu.Data
+{
+    public class WebMenuContext : DbContext
+    {
+        public WebMenuContext (DbContextOptions<WebMenuContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebMenu.Models.Dish> Dish { get; set; }
+    }
+}
---
+++ WebMenu/Models/Dish.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMenu.Models
+{
+    public class Dish
+    {
+        public int Id { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Composition { get; set; }
+        public int Price { get; set; }
+        public int Grammar { get; set; }
+        public int Calorie { get; set; }
+        public DateTime CookingTime { get; set; }
+    }
+}
